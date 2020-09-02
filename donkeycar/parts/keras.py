ee329c342ea78c702583faec76ed874f5dc11af7
Dmitry Koptelov <dimitar98@list.ru>
2020-09-02 20:17:46 +0000

    added new model

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -20,6 +20,7 @@ from tensorflow.python import keras
 from tensorflow.python.keras.layers import Input, Dense
 from tensorflow.python.keras.models import Model, Sequential
 from tensorflow.python.keras.layers import Convolution2D, MaxPooling2D, Reshape, BatchNormalization
+from tensorflow.python.keras.layers import GlobalMaxPooling2D, GlobalAveragePooling2D, Concatenate
 from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Cropping2D, Lambda
 from tensorflow.python.keras.layers.merge import concatenate
 from tensorflow.python.keras.layers import LSTM
@@ -168,6 +169,28 @@ class KerasLinear(KerasPilot):
         throttle = outputs[1]
         return steering[0][0], throttle[0][0]
 
+    
+class KerasLinearNew(KerasPilot):
+    '''
+    The KerasLinear pilot uses one neuron to output a continous value via the 
+    Keras Dense layer with linear activation. One each for steering and throttle.
+    The output is not bounded.
+    '''
+    def __init__(self, num_outputs=2, input_shape=(120, 160, 3), roi_crop=(0, 0), *args, **kwargs):
+        super(KerasLinearNew, self).__init__(*args, **kwargs)
+        self.model = default_n_linear_new(num_outputs, input_shape, roi_crop)
+        self.compile()
+
+    def compile(self):
+        self.model.compile(optimizer=self.optimizer,
+                loss='mse')
+
+    def run(self, img_arr):
+        img_arr = img_arr.reshape((1,) + img_arr.shape)
+        outputs = self.model.predict(img_arr)
+        steering = outputs[0]
+        throttle = outputs[1]
+        return steering[0][0], throttle[0][0]
 
 
 class KerasIMU(KerasPilot):
@@ -350,6 +373,49 @@ def default_n_linear(num_outputs, input_shape=(120, 160, 3), roi_crop=(0, 0)):
     
     return model
 
+
+def default_n_linear_new(num_outputs, input_shape=(120, 160, 3), roi_crop=(0, 0)):
+
+    drop = 0.1
+
+    #we now expect that cropping done elsewhere. we will adjust our expeected image size here:
+    input_shape = adjust_input_shape(input_shape, roi_crop)
+    
+    img_in = Input(shape=input_shape, name='img_in')
+    x = img_in
+    x = Convolution2D(24, (5,5), strides=(2,2), activation='relu', name="conv2d_1")(x)
+    x = Dropout(drop)(x)
+    x = BatchNormalization()(x)
+    x = Convolution2D(32, (5,5), strides=(2,2), activation='relu', name="conv2d_2")(x)
+    x = Dropout(drop)(x)
+    x = BatchNormalization()(x)
+    x = Convolution2D(64, (5,5), strides=(2,2), activation='relu', name="conv2d_3")(x)
+    x = Dropout(drop)(x)
+    x = BatchNormalization()(x)
+    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu', name="conv2d_4")(x)
+    x = Dropout(drop)(x)
+    x = BatchNormalization()(x)
+    x = Convolution2D(64, (3,3), strides=(1,1), activation='relu', name="conv2d_5")(x)
+    x = Dropout(drop)(x)
+    x = BatchNormalization()(x)
+    
+    max_pool = GlobalMaxPooling2D()(x)
+    avg_pool = GlobalAveragePooling2D()(x)
+    
+    x = Concatenate(axis=-1)([max_pool, avg_pool])
+    x = Dense(100, activation='relu')(x)
+    x = Dropout(drop)(x)
+    x = Dense(50, activation='relu')(x)
+    x = Dropout(drop)(x)
+
+    outputs = []
+    
+    for i in range(num_outputs):
+        outputs.append(Dense(1, activation='linear', name='n_outputs' + str(i))(x))
+        
+    model = Model(inputs=[img_in], outputs=outputs)
+    
+    return model
 
 
 def default_imu(num_outputs, num_imu_inputs, input_shape, roi_crop=(0, 0)):
